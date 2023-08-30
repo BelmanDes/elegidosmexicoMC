263f6218f1bd45b1e555bdbc7df9264ade82cb7a
BelmanDes <mlbelman@gmail.com>
2023-08-30 17:00:08 -0600

    Se mofifica la estructura y se agrega la administracion del sitio

---
+++ administracion/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="./imagenes/ElegidosFull.ico">
+        <title>ELEGIDOS MEXICO MC</title>
+        <link rel="stylesheet" href="./css/bootstrap.css"/>
+    </head>
+    <body>
+        
+        <br />
+        <br />
+        <div class="container">
+            <br />
+            <center>
+                <img src="imagenes/FULLPACHT.png" width="300" height="300" alt="Full Patcht" class="img-responsive">
+            </center>
+            <br />
+            <div class="container-cuerpo">
+                
+            </div>
+        </div>
+        <br />
+        <footer class="panel-footer">
+                    <p class="text-center">
+                        <em><strong>Pagina Desarrollada por <a href="http://www.bbsolutions.esy.es">B & B Technology Solutions.</a></strong></em>
+                    </p>
+                    <p class="text-center">
+                        <em><strong>Contactanos: bbsolutions.contacto@gmail.com</strong></em>
+                    </p>
+        </footer>
+        <br />
+    </body>
+</html>
