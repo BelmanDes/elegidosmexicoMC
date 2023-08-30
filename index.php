ec53e081ddb0530d9576524295acac9cfc703cf4
BelmanDes <mlbelman@gmail.com>
2023-08-30 15:28:20 -0600

    Se mofifica el index

--- index.php
+++ index.php
@@ -56,12 +56,20 @@
         <br />
         <div class="container-cuerpo">
             <br />
-            <center>
-                <img src="imagenes/FULLPACHT.png" width="300" height="300" alt="Full Patcht" class="img-responsive">
-            </center>
-            <br />
             <div class="row">
-            
+                <center>
+                    <img src="imagenes/FULLPACHT.png" width="300" height="300" alt="Full Patcht" class="img-responsive">
+                </center>
+                <br />
+                <div class="jumbotron">
+                    <h1 class="display-3">Jumbo heading</h1>
+                    <p class="lead">Jumbo helper text</p>
+                    <hr class="my-2">
+                    <p>More info</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                    </p>
+                </div>
             </div>
         </div>
     </body>
