fa41570b2a03923213a8c743200d1baca0e6f7ac
BelmanDes <mlbelman@gmail.com>
2023-08-30 16:25:13 -0600

    Se mofifica el archivo nosotros.php

--- nosotros.php
+++ nosotros.php
@@ -1,42 +1,43 @@
 <?php include("template/cabecera.php"); ?>
                 <div class="jumbotron">
-                    <h1 class="display-3">Nosotros</h1>
-                    <p class="lead">Este Moto Club Nace.....</p>
-                    <hr class="my-2">
-                    <p>More info</p>
+                    <h1 class="display-3">ELEGIDOS M&Eacute;XICO MC</h1>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-                    </p>
-                </div>
-                <br />
-                <div class="jumbotron">
-                    <h1 class="display-3">Mision</h1>
-                    <p class="lead">Nuestra Mision es</p>
+                        Esun grupo de Motociclistas de Estado de M&eacute;xico; constituido como una organizaci&oacute; sin fines de lucro, 
+                        que nos reunimos por el simple hecho de disfrutar y compartir un hobbie familiar las motos.
+                        </p>
                     <hr class="my-2">
-                    <p>More info</p>
-                    <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-                    </p>
                 </div>
                 <br />
                 <div class="jumbotron">
                     <h1 class="display-3">Vision</h1>
-                    <p class="lead">Nuestra Vision es</p>
-                    <hr class="my-2">
-                    <p>More info</p>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                        Nuestra Vision es ser un moto club familiar con crecimiento sostenido, bas&aacute;ndonos en nuestros valores y en las buenas 
+                        costumbres, el respeto y las ganas de establecer una diferencia dentro de nuestro medio.
+                    </p>
+                </div>
+                <br />
+                <div class="jumbotron">
+                    <h1 class="display-3">Misi&oacute;n</h1>
+                    <p class="lead">
+                        Nuestra Mision es congregar y establecer un gran equipo heterog&eacute;neo de motociclistas que deseen compartir su hobbie de las 
+                        motocicletas dentro de un ambiente familiar lleno de lealtad y compañerismo, respetando ciertas normativas  en pro del beneficio de 
+                        todos a fin de disfrutar al maximo de paseos, encuentros, eventos y dem&aacute;s actividades relacionadas con el motociclismo.
                     </p>
                 </div>
                 <br />
                 <div class="jumbotron">
                     <h1 class="display-3">Valores</h1>
-                    <p class="lead">Nuestros Valores son</p>
-                    <hr class="my-2">
-                    <p>More info</p>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                        Nuestros Valores son: 
+                        FAMILIA
+                        LEALTAD
+                        HERMANDAD
+                        HONESTIDAD
+                        CONSTANCIA
+                        PASIÓN
+
                     </p>
+                    <hr class="my-2">
                 </div>
                 <br />
 <?php include("template/pie.php"); ?>   
