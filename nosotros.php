5fe870cc0637e286e973b0d414e46f606ec0d4c5
BelmanDes <mlbelman@gmail.com>
2023-08-30 16:28:02 -0600

    Se mofifica el archivo nosotros.php

--- nosotros.php
+++ nosotros.php
@@ -14,6 +14,7 @@
                         Nuestra Vision es ser un moto club familiar con crecimiento sostenido, bas&aacute;ndonos en nuestros valores y en las buenas 
                         costumbres, el respeto y las ganas de establecer una diferencia dentro de nuestro medio.
                     </p>
+                    <hr class="my-2">
                 </div>
                 <br />
                 <div class="jumbotron">
@@ -23,20 +24,18 @@
                         motocicletas dentro de un ambiente familiar lleno de lealtad y compañerismo, respetando ciertas normativas  en pro del beneficio de 
                         todos a fin de disfrutar al maximo de paseos, encuentros, eventos y dem&aacute;s actividades relacionadas con el motociclismo.
                     </p>
+                    <hr class="my-2">
                 </div>
                 <br />
                 <div class="jumbotron">
                     <h1 class="display-3">Valores</h1>
-                    <p class="lead">
-                        Nuestros Valores son: 
-                        FAMILIA
-                        LEALTAD
-                        HERMANDAD
-                        HONESTIDAD
-                        CONSTANCIA
-                        PASIÓN
-
-                    </p>
+                    <p class="lead">Nuestros Valores son: </p>
+                    <p class="lead"> FAMILIA</p>
+                    <p class="lead">LEALTAD</p>
+                    <p class="lead">HERMANDAD</p>
+                    <p class="lead">HONESTIDAD</p>
+                    <p class="lead">CONSTANCIA</p>
+                    <p class="lead">PASIÓN</p>
                     <hr class="my-2">
                 </div>
                 <br />
