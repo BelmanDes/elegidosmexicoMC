805c2e8e6ab1bd6fa423098a5d0741f9f683c02e
BelmanDes <mlbelman@gmail.com>
2023-08-30 17:57:55 -0600

    Se mofifica la estructura y se agrega la administracion del sitio

--- administracion/index.php
+++ administracion/index.php
@@ -1,35 +1,44 @@
-<!DOCTYPE html>
+><!doctype html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="../imagenes/ElegidosFull.ico">
-        <title>ELEGIDOS MEXICO MC</title>
-        <link rel="stylesheet" href="../css/bootstrap.css"/>
-    </head>
-    <body>
-        
-        <br />
-        <br />
-        <div class="container">
-            <br />
-            <center>
-                <img src="../imagenes/FULLPACHT.png" width="300" height="300" alt="Full Patcht" class="img-responsive">
-            </center>
-            <br />
-            <div class="container-cuerpo">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Login
+                    </div>
+                    <div class="card-body">
+                        <form>
+                        <div class = "form-group">
+                        <label for="exampleInputEmail1">Usuario</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                        <label for="exampleInputPassword1">Contraseña</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign In</button>
+                        </form>
+                        
+                        
+                    </div>
+                </div>
+                
             </div>
+            
         </div>
-        <br />
-        <footer class="panel-footer">
-                    <p class="text-center">
-                        <em><strong>Pagina Desarrollada por <a href="http://www.bbsolutions.esy.es">B & B Technology Solutions.</a></strong></em>
-                    </p>
-                    <p class="text-center">
-                        <em><strong>Contactanos: bbsolutions.contacto@gmail.com</strong></em>
-                    </p>
-        </footer>
-        <br />
-    </body>
+    </div>
+   
+  </body>
 </html>
