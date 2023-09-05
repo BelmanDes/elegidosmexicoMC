21088d25272c4385f81e00a7320c71dbda5a19c4
BelmanDes <mlbelman@gmail.com>
2023-09-05 17:48:24 -0600

    Se mofifica la estructura y se agrega la administracion del sitio

--- administracion/template/cabecera.php
+++ administracion/template/cabecera.php
@@ -11,13 +11,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <?php $url="http://".$_SERVER['HTTP_HOST']."/elegidosmexicoMC" ?>
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <a class="nav-item nav-link active" href="#">Administrador de Miembros EMMC<span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Inicio</a>
-            <a class="nav-item nav-link" href="#">Miembros</a>
-            <a class="nav-item nav-link" href="#">Motos</a>
-            <a class="nav-item nav-link" href="#">Contactos</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/index.php">Inicio</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/seccion/miembros.php">Miembros</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/seccion/motos.php">Motos</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/seccion/contactos.php">Contactos</a>
+            <a class="nav-item nav-link" href="#">Cerrar Sesion</a>
         </div>
     </nav>
     </nav>
