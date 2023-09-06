d1ac504d808cd5038f4a4fad208fc7d9b45b1e42
BelmanDes <mlbelman@gmail.com>
2023-09-05 18:33:27 -0600

    Se mofifica la estructura y se agrega la administracion del sitio

--- administracion/template/cabecera.php
+++ administracion/template/cabecera.php
@@ -13,46 +13,36 @@
   <body>
     <?php $url="http://".$_SERVER['HTTP_HOST']."/elegidosmexicoMC" ?>
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Administrador de Miembros EMMC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="<?php echo $url;?>/administracion/inicio.php">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $url;?>/administracion/seccion/miembros.php">Miembros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Motos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contactos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Cerrar Sesion</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="search" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    <!--<nav class="navbar navbar-expand navbar-light bg-light">
-        <div class="nav navbar-nav">
-            <a class="nav-item nav-link active" href="#">Administrador de Miembros EMMC<span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/inicio.php">Inicio</a>
-            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/seccion/miembros.php">Miembros</a>
-            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/seccion/motos.php">Motos</a>
-            <a class="nav-item nav-link" href="<?php echo $url;?>/administracion/seccion/contactos.php">Contactos</a>
-            <a class="nav-item nav-link" href="#">Cerrar Sesion</a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Administrador de Miembros EMMC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="<?php echo $url;?>/administracion/inicio.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url;?>/administracion/seccion/miembros.php">Miembros</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url;?>/administracion/seccion/motos.php">Motos</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url;?>/administracion/seccion/contactos.php">Contactos</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url;?>/administracion/seccion/cerrar.php">Cerrar Sesion</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-sm-2" type="search" placeholder="Search">
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-    </nav-->
+    </nav>
     <div class="container">
         <br />
         <br />
